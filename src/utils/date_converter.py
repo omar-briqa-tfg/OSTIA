ccef6427399157399ebd3ec13ba518f2af99184b
Omar Briqa <58139559+OBriqa@users.noreply.github.com>
2024-03-02 12:25:47 +0100

    feat(utils): add date_converter utils (#4)

---
+++ src/utils/date_converter.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+
+def to_iso_format(date: str, time: str) -> (str, str):
+
+    date_iso = datetime.strptime(date, '%d/%b/%Y').date().isoformat()
+    time_iso = datetime.strptime(time, '%H:%M:%S %z').time().isoformat()
+
+    return date_iso, time_iso
+
+
+def to_timestamp(date: str, time: str) -> int:
+
+    return int(datetime.strptime(date + ' ' + time, '%d/%b/%Y %H:%M:%S %z').timestamp())
