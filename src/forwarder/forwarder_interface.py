c878c19a9c311e9f5630430ad73c17b2a353268f
OBriqa <omarbriqa@gmail.com>
2024-03-16 15:10:28 +0100

    Update src/forwarder | Add forwarder interface

---
+++ src/forwarder/forwarder_interface.py
@@ -0,0 +1,17 @@
+from src.log import Log
+
+from typing import List
+
+from abc import ABC, abstractmethod
+
+class IFilter(ABC):
+
+    #TODO: replace int by Response class
+
+    @abstractmethod
+    def forward(self, log: Log) -> int:
+        pass
+
+    @abstractmethod
+    def forwardMany(self, logs: List[Log]) -> int:
+        pass
