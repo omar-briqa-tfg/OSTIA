1818ecc57ffe1a125c6ee403e048759cc2cbabfb
OBriqa <omarbriqa@gmail.com>
2024-05-01 17:14:00 +0200

    feat(Filter): Add WithIPv6Address

---
+++ src/filter/with_ipv6address.py
@@ -0,0 +1,11 @@
+from src.filter.filter_interface import IFilter
+
+import re
+
+class WithIPv6Address(IFilter):
+
+    IPV6_PATTERN = r"^([a-fA-F0-9:]+|[uU]nknown), "
+
+    @classmethod
+    def filter(self, log: str) -> bool:
+        return bool(re.search(self.IPV6_PATTERN, log))
--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
-from src.filter.without_ipaddress import WithoutIpAddress
+from src.filter.without_ipaddress   import WithoutIpAddress
+from src.filter.with_ipv6address    import WithIPv6Address
 
 log = "111.11.11.111 - - [31/Mar/2023:00:00:01 +0200]"
 
 _ = WithoutIpAddress.filter(log)
+
+_ = WithIPv6Address.filter(log)
