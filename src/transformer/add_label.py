3ef39a08f2d8861225512480be45a26936504d3b
OBriqa <omarbriqa@gmail.com>
2024-05-01 19:38:35 +0200

    fix(AddLabel): dont return anything

--- src/transformer/add_label.py
+++ src/transformer/add_label.py
@@ -3,6 +3,5 @@ from src.transformer.transformer_interface import ITransformer
 class AddLabel(ITransformer):
 
     @classmethod
-    def transform(cls, log: dict, label: str, value: str) -> dict:
+    def transform(cls, log: dict, label: str, value: str) -> None:
         log[label] = value
-        return log
