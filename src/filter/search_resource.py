8a7133dcd2b02609021f88d19b7810a4cfba458e
OBriqa <omarbriqa@gmail.com>
2024-05-02 16:21:21 +0200

    feat(SearchResource): add search keys

--- src/filter/search_resource.py
+++ src/filter/search_resource.py
@@ -3,7 +3,7 @@ from src.filter.filter_interface import IFilter
 class SearchResource(IFilter):
 
     #TODO: complete list
-    SEARCH_KEYS = ['discover', 'search', 'browse', 'open-search']
+    SEARCH_KEYS = ['discover?', 'search?', 'browse?', 'open-search?']
 
     @classmethod
     def filter(cls, resource: str) -> bool:
