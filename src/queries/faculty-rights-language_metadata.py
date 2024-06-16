d9d607f10097fb30515ba7616f69a897cbce6746
OBriqa <omarbriqa@gmail.com>
2024-06-16 23:51:07 +0200

    Update faculty-rights-language_metadata

--- src/queries/faculty-rights-language_metadata.py
+++ src/queries/faculty-rights-language_metadata.py
@@ -1,8 +1,15 @@
+# Disclaimer
+# This is a PoC trial code aiming to demo the interoperability between the logs and the metadata
+# In order to make the analysis as eficient as possible, certain design decisions must be updated.
+# get_metadata_rights_value and get_metadata_language_value should be O(1)
+# InfluxDb write api should change to batching mode as it is in the InfluxDB log forwarder
+
 from influxdb_client import InfluxDBClient
 
 from pymongo import MongoClient
 
 import os
+import re
 import ast
 
 influxdb_org = os.environ.get('INFLUXDB_ORG')
@@ -14,25 +21,83 @@ mongodb_url = os.environ.get('MONGODB_URL')
 mongodb_database_name = os.environ.get('MONGODB_DATABASE')
 mongodb_collection_name = os.environ.get('MONGODB_COLLECTION')
 
-resources = {}
-influxDbClient = InfluxDBClient(url=influxdb_url, token=influxdb_token).query_api()
+influxDbClientRead  = InfluxDBClient(
+    url=influxdb_url,
+    token=influxdb_token).query_api()
+
+influxDbClientWrite = InfluxDBClient(
+    url=influxdb_url,
+    token=influxdb_token,
+    enable_gzip=True).write_api()
 
 mongoDbClient = MongoClient(mongodb_url)
 mongoDbCollection = mongoDbClient[mongodb_database_name][mongodb_collection_name]
 
-for day in range(2, 3):
+rights_regex = re.compile(r"^dc-rights-access")
+language_regex = re.compile(r"^dc-language-iso")
 
-    # |> range(start: 2023-12-{day-1:02}T00:00:00Z, stop: 2023-12-{day:02}T00:00:00Z)
+def get_metadata_rights_value(metadata: dict):
+
+    for key in metadata:
+        if rights_regex.match(key):
+            return metadata[key]
+
+    return None
+
+def get_metadata_language_value(metadata: dict):
+
+    for key in metadata:
+        if language_regex.match(key):
+            return metadata[key]
+
+    return None
+
+def push_entry_influxdb(handle, rights, language, time):
+
+    language = [language] if isinstance(language, str) else language
+
+    for lang in language:
+
+        data = {
+            'measurement': 'epsevg-language',
+            'tags': {
+                'lang': lang
+            },
+            'fields': {
+                'recurs': handle,
+            },
+            'time': int(time.timestamp() * 1e9)
+        }
+
+        influxDbClientWrite.write(record=data, write_precision='ns', org=influxdb_org, bucket=influxdb_bucket)
+
+
+    data = {
+        'measurement': 'epsevg-rights',
+        'tags': {
+            'rights': rights,
+        },
+        'fields': {
+            'recurs': handle,
+        },
+        'time': int(time.timestamp() * 1e9)
+    }
+
+    influxDbClientWrite.write(record=data, write_precision='ns', org=influxdb_org, bucket=influxdb_bucket)
+
+FACULTY_KEYWORD = "vilanova"
+for day in range(2, 32):
 
     query = f"""
     from(bucket: "upcommons")
-    |> range(start: 2023-12-01T00:00:00Z, stop: 2023-12-01T00:01:00Z)
+    |> range(start: 2023-12-{day-1:02}T00:00:00Z, stop: 2023-12-{day:02}T00:00:05Z)
     |> filter(fn: (r) => r["_measurement"] == "tfg")
     |> filter(fn: (r) => r["_field"] == "recurs")
     |> keep(columns: ["_value", "_time"])
     """
+    print(f"2023-12-{day-1:02}")
 
-    results = influxDbClient.query(query=query, org=influxdb_org)
+    results = influxDbClientRead.query(query=query, org=influxdb_org)
 
     for result in results:
         for record in result.records:
@@ -40,11 +105,24 @@ for day in range(2, 3):
             time = record.get_time()
 
             values = ast.literal_eval(ids) if '[' in ids else [ids]
-            for value in values:
+            for handle in values:
 
-                mongo_query = {
-                    'id': value
-                }
+                mongo_query = { 'id': handle }
                 resource = mongoDbCollection.find_one(mongo_query)
 
-                print(resource)
+                if resource:
+                    metadata = resource.get('metadata', {})
+                    faculties = metadata.get('dc-audience-mediator')
+
+                    if faculties:
+                        faculties = [faculties] if isinstance(faculties, str) else faculties
+                        for faculty in faculties:
+                            if FACULTY_KEYWORD in faculty.lower():
+                                rights = get_metadata_rights_value(metadata)
+                                language = get_metadata_language_value(metadata)
+
+                                push_entry_influxdb(handle, rights, language, time)
+
+
+
+mongoDbClient.close()
